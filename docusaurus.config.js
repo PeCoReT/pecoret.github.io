05fba43f991374936c5117a114062d59a6563aa8
blocki <62061776+blockisec@users.noreply.github.com>
2023-10-07 15:41:31 +0200

    dark theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,6 +63,10 @@ const config = {
         ({
             // Replace with your project's social card
             image: 'img/docusaurus-social-card.jpg',
+            colorMode: {
+              defaultMode: 'dark',
+              disableSwitch: true
+            },
             navbar: {
                 title: 'PeCoReT',
                 logo: {
